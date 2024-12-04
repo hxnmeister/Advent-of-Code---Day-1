83169d0a4ce26ce0e72dc851346419849fe3ecea
Valerii <valeriy.stronskiy@gmail.com>
2024-12-04 08:36:55 +0200

    Part One

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Advent_of_Code___Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH_TO_INPUT = @"G:\Visual Studio Projects\Advent of Code - Day 1\input.txt";
+
+            int totalSum = 0;
+
+            List<int> leftValues = new List<int>();
+            List<int> rightValues = new List<int>();
+
+            foreach (string line in File.ReadAllLines(PATH_TO_INPUT))
+            {
+                string[] temp = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                leftValues.Add(Int32.Parse(temp[0]));
+                rightValues.Add(Int32.Parse(temp[1]));
+            }
+
+            leftValues.Sort();
+            rightValues.Sort();
+
+            for (int i = 0; i < leftValues.Count; i++)
+            {
+                totalSum += Math.Abs(leftValues[i] - rightValues[i]);
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
